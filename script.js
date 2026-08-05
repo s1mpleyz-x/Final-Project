c050efc59203d205cd14db2fa2f5b18b08b69168
Sammy Kariuki <simpleyzrx@gmail.com>
2026-08-05 16:55:39 +0300

    script(modified):added an event handler for clicks of the cards

---
+++ script.js
@@ -0,0 +1,13 @@
+// ------------------------------ CARD CLICK EVENT HANDLER ------------------------------ \\
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+    card.addEventListener("click", function(event) {
+        event.preventDefault(); // Stops the browser from reloading each time a card is clicked
+        console.log("A card was clicked!"); // Confirms a card was clicked
+
+        let cardClicked = event.currentTarget.id;
+        console.log(cardClicked);
+    })
+})
